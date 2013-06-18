b5c22743aa177427165d3161d908ec0699877916
mugijiru <mugijiru.dev@gmail.com>
2013-06-18 17:24:18 +0900

    ライセンス表記等を追加

--- fuelphp-mode.el
+++ fuelphp-mode.el
@@ -1,5 +1,29 @@
-;(add-to-list 'load-path (expand-file-name "~/Projects/git/hoge/fuelphp-mode"))
-;(require 'fuelphp-mode)
+;;; fuelphp-mode.el --- Minor mode for FuelPHP project
+
+;; Copyright (C) 2013 by Mugijiru
+
+;; Author: Mugijiru <mugijiru.dev@gmail.com>
+;; URL: https://github.com/mugijiru/fuelphp-mode
+;; Version: 0.01
+;; Package-Requires: (cl)
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 
 (require 'cl)
 (defvar fuelphp-mode-map
@@ -167,3 +191,5 @@ If not FuelPHP directory, then return nil."
   :lighter " fuelphp")
 
 (provide 'fuelphp-mode)
+
+;;; fuelphp-mode.el ends here
